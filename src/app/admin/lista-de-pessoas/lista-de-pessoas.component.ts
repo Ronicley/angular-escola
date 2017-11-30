d3f19a799ac6b7f9a9b2f2384a586cb2f725b33e
Ronicley Silva <ronicleysantossilva@gmail.com>
2017-11-30 16:32:23 -0300

    Angular-escola

---
+++ src/app/admin/lista-de-pessoas/lista-de-pessoas.component.ts
@@ -0,0 +1,39 @@
+import { PessoasService } from './../pessoas.service';
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-lista-de-pessoas',
+  templateUrl: './lista-de-pessoas.component.html',
+  styleUrls: ['./lista-de-pessoas.component.css']
+})
+export class ListaDePessoasComponent implements OnInit {
+  excluir_ok = false;
+  excluir_erro = false;
+  pessoas = [];
+
+  constructor(private pessoasService: PessoasService) {
+  }
+
+  ngOnInit() {
+    this.atualizarLista();
+  }
+
+  excluir(pessoa) {
+    if (confirm('Tem certeza que deseja excluir a pessoa ' + pessoa.nome + '?')) {
+      this.pessoasService.deletePessoa(pessoa.id)
+        .subscribe(ok => {
+          this.excluir_ok = true;
+          this.excluir_erro = false;
+          this.atualizarLista();
+        }, erro => {
+          this.excluir_ok = false;
+          this.excluir_erro = true;
+        });
+    }
+  }
+
+  atualizarLista() {
+    this.pessoas.concat(this.pessoasService.getPessoas()
+      .subscribe(pessoas => this.pessoas = pessoas));
+  }
+}
---
+++ src/app/admin/pessoa/pessoa.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+import {PessoasService} from "../pessoas.service";
+import {ActivatedRoute, Router} from "@angular/router";
+
+@Component({
+  selector: 'app-pessoa',
+  templateUrl: './pessoa.component.html',
+  styleUrls: ['./pessoa.component.css']
+})
+export class PessoaComponent implements OnInit {
+  pessoa;
+
+  constructor(private PessoasService: PessoasService, private route: ActivatedRoute, private router: Router) {
+  }
+
+  ngOnInit() {
+    const id = parseInt(this.route.snapshot.paramMap.get('id'));
+    this.PessoasService.getPessoa(id).subscribe(pessoa => this.pessoa = pessoa);
+  }
+
+}
